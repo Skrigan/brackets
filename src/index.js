7779e68ec292b448713054754ffbab304d449b93
Skrigan Dmitry <sk_dm-821@mail.ru>
2022-08-10 13:32:31 +0300

    ; )

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+  if (str.length % 2) return false;
+
+  let stack = [];
+
+  for (let i = 0; i < str.length; i++) {
+      for (let item of bracketsConfig) {
+          let stackTop = stack[stack.length - 1];
+          if (str[i] === item[0]) {
+              stack.push(str[i]);
+          } else if(str[i] === item[1] && stackTop === item[0]) {
+              stack.pop();
+          } 
+      }
+  }
+
+  return stack.length === 0;
+
 }
