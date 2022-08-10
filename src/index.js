f4b35da55958bcdad0323d01676e235f1be27fa1
Skrigan Dmitry <sk_dm-821@mail.ru>
2022-08-10 13:35:04 +0300

    ; )

--- src/index.js
+++ src/index.js
@@ -1,20 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-
   if (str.length % 2) return false;
-
   let stack = [];
-
   for (let i = 0; i < str.length; i++) {
       for (let item of bracketsConfig) {
           let stackTop = stack[stack.length - 1];
-          if (str[i] === item[0]) {
-              stack.push(str[i]);
-          } else if(str[i] === item[1] && stackTop === item[0]) {
+          if(str[i] === item[1] && stackTop === item[0]) {
               stack.pop();
-          } 
+          } else if(str[i] === item[0]) {
+              stack.push(str[i]);
+          }
       }
   }
-
   return stack.length === 0;
-
 }
